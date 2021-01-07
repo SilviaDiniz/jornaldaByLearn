44af1ea64a9ddbb6807a3c42e49c871eb76ba6f0
SILVIA REGINA ROLON DINIZ <39505300+SilviaDiniz@users.noreply.github.com>
2021-01-07 12:19:59 -0300

    Segundo comite

--- imc.py
+++ imc.py
@@ -1,7 +1,22 @@
+# Calculo IMC
+def numero_quadrado(numero):
+  quadrado = numero * numero
+  return quadrado
+
+def imc(peso, altura):
+  altura_quadrada = numero_quadrado(altura)
+  meu_imc = peso / altura_quadrada
+  return meu_imc
+
+meu_imc = imc(86, 1.56)
+print(f'O meu imc é {meu_imc}')
+
+
+# Calculo IMC refaturado
 def imc(peso, altura):
   altura_quadrada = altura ** 2
   meu_imc = peso / altura_quadrada
   return meu_imc
 
 meu_imc = imc(86, 1.56)
-print(f'O meu imc � {meu_imc: .2f}')
+print(f'O meu imc é {meu_imc: .2f}')
